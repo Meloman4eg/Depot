1697215e1637c991c2af000f250ced0e58112ce0
Pavel Dadykin <pavel.splean@gmail.com>
2014-07-10 21:47:31 +0200

    Added Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true, length: {minimum: 10}
@@ -10,4 +13,17 @@ class Product < ActiveRecord::Base
   def self.latest
     Product.order(:updated_at).last
   end
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      true
+    else
+      errors.add(:base, 'существуют товарные позиции')
+      false
+    end
+
+  end
+
 end
